dc08f9cb19f330d5bf41b10cac2ab5cf7f07d07a
torinunna <torinunna9588@gmail.com>
2022-08-20 00:48:21 +0900

    add buttons and navigation controller to Main.storyboard, create SeguePushViewController

---
+++ ScreenTransitionPractice/SeguePushViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SeguePushViewController.swift
+//  ScreenTransitionPractice
+//
+//  Created by YUJIN KWON on 2022/08/20.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
