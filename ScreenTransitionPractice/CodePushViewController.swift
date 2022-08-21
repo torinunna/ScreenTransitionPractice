e4ccee49c8ffe5a7bf13a18b632cff2d36b21888
torinunna <torinunna9588@gmail.com>
2022-08-21 23:27:42 +0900

    create CodePushVC, implement push by code and backPressed action

---
+++ ScreenTransitionPractice/CodePushViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CodePushViewController.swift
+//  ScreenTransitionPractice
+//
+//  Created by YUJIN KWON on 2022/08/21.
+//
+
+import UIKit
+
+class CodePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
